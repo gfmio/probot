c342ea53793f7d0b3ee8337ae3eb60f4ff9ab0a9
Frédérique Mittelstaedt <git@gfm.io>
2025-07-09 15:20:03 +0100

    remove old

--- old/test/deprecations.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe, it } from "vitest";
-
-describe("Deprecations", () => {
-  it("no deprecations exists at this point", () => {});
-});
--- old/test/types/probot.test-d.ts
+++
@@ -1,9 +0,0 @@
-import { RepositoryEditedEvent } from "@octokit/webhooks-types";
-import { expectType } from "tsd";
-import { Probot } from "../../src/index.js";
-
-const app = new Probot({});
-
-app.on("repository.edited", (context) => {
-  expectType<RepositoryEditedEvent>(context.payload);
-});
